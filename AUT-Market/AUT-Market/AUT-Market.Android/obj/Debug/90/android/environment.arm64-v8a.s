5df1e5fa1ffe537bd8c0025c7918aef162af4bca
Karan Patel <njm7162@autuni.ac.nz>
2020-05-20 08:57:10 +1200

    fix the conflit

--- AUT-Market/AUT-Market/AUT-Market.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ AUT-Market/AUT-Market/AUT-Market.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"7a044955-4105-423a-830d-cb28f75c9444"
+	.asciz	"689cd4fa-8c9f-4e84-8650-610b2e0e23c7"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
