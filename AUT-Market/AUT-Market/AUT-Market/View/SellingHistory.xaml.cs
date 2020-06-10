5a3712654102c622abc308ca386d6dc5b9922fb0
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2020-06-10 15:31:51 +1200

    Revert "UPDATE: seller history is change to comment  code ."
    This reverts commit e0a6dd24da024bc7a5e954beeb6f29b54daf1aef.

--- AUT-Market/AUT-Market/AUT-Market/View/SellingHistory.xaml.cs
+++ AUT-Market/AUT-Market/AUT-Market/View/SellingHistory.xaml.cs
@@ -1,26 +1,26 @@
-﻿//using AUT_Market.ViewModel;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using AUT_Market.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//using Xamarin.Forms;
-//using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-//namespace AUT_Market.View
-//{
-//    [XamlCompilation(XamlCompilationOptions.Compile)]
-//    public partial class SellingHistory : ContentPage
-//    {
-//        SellingHistoryViewModel vm;
-//        public SellingHistory()
-//        {
-//            InitializeComponent();
+namespace AUT_Market.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SellingHistory : ContentPage
+    {
+        SellingHistoryViewModel vm;
+        public SellingHistory()
+        {
+            InitializeComponent();
 
-//            vm = new SellingHistoryViewModel();
+            vm = new SellingHistoryViewModel();
 
-//            BindingContext = vm;
-//        }
-//    }
-//}
+            BindingContext = vm;
+        }
+    }
+}
