dc02a51736c36fc46cb848d6a6163786b3f10cdd
Menglong Yao <chr7357@autuni.ac.nz>
2020-06-28 17:23:37 +1200

    Oliver Update: New Server for image
    the old server for saving images is expired, new one has been updated.

--- AUT-Market/AUT-Market/AUT-Market/Service/BaseServer.cs
+++ AUT-Market/AUT-Market/AUT-Market/Service/BaseServer.cs
@@ -11,10 +11,12 @@ namespace AUT_Market.Service
 {
     public class BaseServer
     {
-        public static string baseurl = "http://qb3ggg6va.bkt.clouddn.com/";
+        public static string baseurl = "http://qcm4kzcjt.bkt.clouddn.com/";// qb3ggg6va.bkt.clouddn.com
         public static string AccessKey = "3btf_FpEEgt0_6U1kND-EG0s8TWGII9YC-6M6YIe";
         public static string SecretKey = "IEd4e36dMIIy-HbuktCvvbPZHp_FJVL9KmSbJ46x";
-        public static string Bucket = "autmarket";
+        public static string Bucket = "autmarket1";
+
+
         public async static Task<Dictionary<string, string>> UpdateImage(string filePath) {
             var dic = new Dictionary<string, string>()
             {
